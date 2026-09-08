0be9bc452c58ddbc98005dfe8b6aea262f762504
WilliamBarbosaDev <williambdesigner@gmail.com>
2026-09-07 23:59:40 -0400

    fix(server): force dev: false and NODE_ENV: production in server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const { createServer } = require("http");
 const { parse } = require("url");
 const next = require("next");
 
-const dev = process.env.NODE_ENV !== "production";
+process.env.NODE_ENV = "production";
+const dev = false;
 const hostname = process.env.HOSTNAME || "localhost";
 const port = parseInt(process.env.PORT || "3000", 10);
 
